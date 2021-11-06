40a6497e951f82c7772d5d5307e14b6915afd316
binyomen <binyomen@users.noreply.github.com>
2021-11-06 01:57:17 +0000

    deploy: 2cb08d67180125a9240d4031ee4d5de0632a76c6

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"loxide":{"doc":"","t":[5],"n":["test"],"q":["loxide"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["loxide"] = {"name":"","files":["chunk.rs","lib.rs"]};
+createSourceSidebar();
