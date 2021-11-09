15c54e9c9c03081586b7fd6c94837fa5d904be6d
Ben Weedon <ben@weedon.email>
2021-11-08 21:57:18 -0800

    Add tests for ValueStack

--- src/vm.rs
+++ src/vm.rs
@@ -22,6 +22,14 @@ struct ValueStack {
 }
 
 impl ValueStack {
+    fn new() -> Self {
+        const EMPTY_VALUE: Option<Value> = None;
+        Self {
+            data: [EMPTY_VALUE; STACK_SIZE],
+            index: 0,
+        }
+    }
+
     fn push(&mut self, value: Value) {
         if self.index == STACK_SIZE {
             panic!("Stack overflow!");
@@ -65,14 +73,10 @@ pub struct Vm<'a> {
 
 impl<'a> Vm<'a> {
     pub fn new(chunk: &'a Chunk) -> Self {
-        const EMPTY_VALUE: Option<Value> = None;
         Self {
             chunk,
             cursor: chunk.cursor(),
-            stack: ValueStack {
-                data: [EMPTY_VALUE; STACK_SIZE],
-                index: 0,
-            },
+            stack: ValueStack::new(),
         }
     }
 
@@ -107,7 +111,7 @@ impl<'a> Vm<'a> {
 
 #[cfg(test)]
 mod tests {
-    use {super::*, crate::value::Value};
+    use {super::*, crate::value::Value, std::panic::catch_unwind};
 
     #[test]
     fn simple_chunk_doesnt_produce_an_error() {
@@ -120,5 +124,79 @@ mod tests {
 
         let mut vm = Vm::new(&chunk);
         assert!(vm.interpret().is_ok());
+    }
+
+    #[test]
+    fn value_stack_can_push_values() {
+        let mut stack = ValueStack::new();
+        stack.push(Value::new(123.0));
+        stack.push(Value::new(69.420));
+
+        assert_eq!(
+            stack.data.iter().take(3).collect::<Vec<&Option<Value>>>(),
+            vec![&Some(Value::new(123.0)), &Some(Value::new(69.420)), &None]
+        );
+    }
+
+    #[test]
+    fn value_stack_can_pop_values() {
+        let mut stack = ValueStack::new();
+        stack.push(Value::new(123.0));
+        stack.push(Value::new(69.420));
+
+        assert_eq!(stack.pop(), Value::new(69.420));
+        assert_eq!(stack.pop(), Value::new(123.0));
+
+        assert_eq!(stack.data.iter().next(), Some(&None));
+    }
+
+    #[test]
+    fn value_stack_can_push_up_to_256_values() {
+        let mut stack = ValueStack::new();
+        for i in 0..256 {
+            stack.push(Value::new(i as f64));
+        }
+
+        assert_eq!(
+            stack
+                .data
+                .iter()
+                .take_while(|v| v.is_some())
+                .fold(0, |acc, _| { acc + 1 }),
+            256
+        );
+    }
+
+    #[test]
+    fn value_stack_will_overflow() {
+        let mut stack = ValueStack::new();
+        for i in 0..STACK_SIZE {
+            stack.push(Value::new(i as f64));
+        }
+
+        assert_eq!(
+            *catch_unwind(move || {
+                stack.push(Value::new(256.0));
+            })
+            .unwrap_err()
+            .downcast_ref::<&str>()
+            .unwrap(),
+            "Stack overflow!"
+        );
+    }
+
+    #[test]
+    fn value_stack_cant_pop_when_empty() {
+        let mut stack = ValueStack::new();
+
+        assert_eq!(
+            *catch_unwind(move || {
+                stack.pop();
+            })
+            .unwrap_err()
+            .downcast_ref::<&str>()
+            .unwrap(),
+            "Cannot pop from an empty stack."
+        );
     }
 }
